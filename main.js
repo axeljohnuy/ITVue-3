e7965de581688183ef8394e82ea3e055e5b6c323
axeljohnuy <axeljohndumaguinguy@gmail.com>
2024-08-17 18:07:26 +0800

    lesson 2-start sample commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Android',
+            description: 'Android is a popularly used open-source operating system used for smartphones and tablet computers.'
+        }
+    }
+})
